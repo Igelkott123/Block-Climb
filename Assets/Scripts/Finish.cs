10e1897cded46fcabac077c0a692461c662a5a99
Igelkott123 <olle.wallberg@bredband.net>
2016-02-06 19:50:02 +0100

    Added a finish board and a level

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.collider.gameObject.tag=="Player")
+		{
+			Application.LoadLevel(0);
+		}
+	}
+}
